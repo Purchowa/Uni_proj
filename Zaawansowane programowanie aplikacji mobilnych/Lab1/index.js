4b775983b6b65f39fd778be1beae92e5b1b66f8e
Dawid_W <purchowa@gmail.com>
2023-10-13 10:43:31 +0200

    ZPSM-L2-Z2 - min, max feature

--- Zaawansowane programowanie aplikacji mobilnych/Lab1/index.js
+++ Zaawansowane programowanie aplikacji mobilnych/Lab1/index.js
@@ -1,5 +1,7 @@
 const _ = require("lodash")
 
-const numbers = [1, 2, 3, 4]
+const numbers = [1, 2, 3, 72, 15, 22, 34]
 
-console.log(_.mean(numbers))
+console.log("Mean: " + _.mean(numbers))
+console.log("Min value: " + _.min(numbers))
+console.log("Max value: " + _.max(numbers))
